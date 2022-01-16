d258bb144e5b5ebd429dab00911a190f134a1c47
g-hannah <46569963+g-hannah@users.noreply.github.com>
2022-01-16 18:05:26 +0000

    Create two_sum.cpp

---
+++ leetcode_solutions/two_sum.cpp
@@ -0,0 +1,53 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ */
+
+class Solution
+{
+    using miter = std::map<int,int>::iterator;
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        std::vector<int> ret;
+        std::map<int,int> map;
+        
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            map.insert(pair<int,int>(target - nums.at(i), i));
+        }
+        
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            const miter other = map.find(nums.at(i));
+            if (map.end() != other && i != other->second)
+            {
+                ret.push_back(i);
+                ret.push_back(other->second);
+                
+                return ret;
+            }
+        }
+        
+        return ret;
+    }
+};
