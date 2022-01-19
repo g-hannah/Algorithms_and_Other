9ee78aa62bbb9b3482251ac7ca49035ba9d521fb
g-hannah <46569963+g-hannah@users.noreply.github.com>
2022-01-19 19:09:01 +0000

    Create add_two_numbers.cpp

---
+++ leetcode_solutions/medium/add_two_numbers.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+    {
+        int val = 0, carry = 0;
+        ListNode *node = new ListNode(), *tmp;
+
+        for (tmp = node; l1 && l2; d *= 10; )
+        {
+            val = l1->val + l2->val + carry;
+            carry = val / 10;
+            tmp->val = val % 10;
+
+            l1 = l1->next;
+            l2 = l2->next;
+
+            if (l1 && l2)
+                tmp->next = new ListNode(), tmp = tmp->next;
+        }
+
+        while (l1)
+        {
+            tmp->next = new ListNode(), tmp = tmp->next;
+            val = l1->val + carry;
+            carry = val / 10;
+            tmp->val = val % 10;
+            l1 = l1->next;
+        }
+
+        while (l2)
+        {
+            tmp->next = new ListNode(), tmp = tmp->next;
+            val = l2->val + carry;
+            carry = val / 10;
+            tmp->val = val % 10;
+            l2 = l2->next;
+        }
+
+        return node;
+    }
+};
